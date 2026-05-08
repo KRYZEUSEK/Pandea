7fa41569833e800158e72fbfa5e1b150f69c7176
userLeftComputer119class <95029525+CyanurdePotassium@users.noreply.github.com>
2026-05-08 14:49:43 +0200

    typewriter

---
+++ Assets/Foldery Prywatne/SP/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // https://github.com/SuperValou/TypewriterEffect
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
